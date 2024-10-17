3529e2ec156ee047f40c8355b1d4567d06e39d5c
Christian Jhegg Fermilan <christianjheggfer@gmail.com>
2024-10-18 02:12:58 +0800

    fix: Auth Layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
-import placeholder from "@/app/auth.png";
+import placeholder from "@/app/favicon.ico";
 export default async function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="w-full lg:grid lg:grid-cols-2 min-h-screen">
-      <div className="flex items-center justify-center py-12">{children}</div>
-      <div className="hidden lg:block">
+    <div className="w-full flex flex-col min-h-screen">
+      <div className="mt-10 flex justify-center">
         <Image
           src={placeholder}
           alt="Image"
-          width="1920"
-          height="1080"
-          className="h-screen w-full object-cover"
+          width="300"
+          height="300"
+          className=""
         />
       </div>
+      <div className="flex items-center justify-center py-12">{children}</div>
     </div>
   );
 }
